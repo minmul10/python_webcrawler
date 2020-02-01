5b303b166aa0bd5c5eb7e7d9bd6ae4ad7bdfc933
minmul10 <minmul10@gmail.com>
2020-02-01 13:03:42 +0900

    class.py
    class 함수

---
+++ class.py
@@ -0,0 +1,19 @@
+class crawler: # 도장파기?
+    a = 10  # property -> override 가능
+    b = 20  # property
+    
+    def add_a_b(self): # class method, # 파이썬 스코프 확인.
+        return self.a + self.b
+
+# inheritance -> 도장파기 , 찍기의 기능 유지?
+# class, 상속, 객체지향 프로그래밍(OOP) -> Python프로그램 형식으로 찾아볼 것.
+
+class 이름():                       #상위클래스
+    def 함수1(Self):
+    
+class 이름_1(이름):                 # 상속받은 클래스(하위클래스)
+    def 함수1(self):                # override 된 함수
+
+
+
+    
